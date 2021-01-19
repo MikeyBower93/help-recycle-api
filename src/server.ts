822f0ae602a7e30def5d062fbe4f504406c29d6d
MikeyBower93 <mikey.bower93@gmail.com>
2021-01-19 19:19:22 +0000

    Up version.

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (_request: express.Request, response: express.Response) => {
-	response.send('hello world');
+	response.send('hello world from beanstalk');
 });
 
 app.listen(
